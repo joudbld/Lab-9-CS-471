eb0579e0a1a8ace5d8ac5b897e071979db84d415
Joud <joud.albelaihed.com>
2025-04-07 14:17:48 +0300

    Initial commit

---
+++ libraryproject1/apps/bookmodule/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for libraryproject1 project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
+    path('html5/links/', views.links, name="books.links"),
+    path('html5/text/formatting/', views.formatting, name="books.formatting"),
+    path('html5/listing/', views.listing, name="books.listing"),
+    path('html5/tables/', views.tables),
+    path('search/', views.search),
+    path('simple/query/', views.simple_query),
+    path('complex/query/', views.complex_query),
+    path('lab8/task1/', views.task1),
+    path('lab8/task2/', views.task2),
+    path('lab8/task3/', views.task3),
+    path('lab8/task4/', views.task4),
+    path('lab8/task5/', views.task5),
+    path('lab8/task7/', views.task7),
+
+
+
+]
--- libraryproject1/apps/bookmodule/views.py
+++ libraryproject1/apps/bookmodule/views.py
@@ -1,3 +1,130 @@
 from django.shortcuts import render
 
+from .models import Book, Address, Student
+from django.db.models import Q
+
 # Create your views here.
+from django.http import HttpResponse
+
+from django.db.models import Count, Min, Max, Sum, Avg
+
+
+def index(request):
+    name = request.GET.get("name") or "world!"
+    return render(request, "bookmodule/index.html" , {"name": name})  #your render line
+
+
+def index2(request, val1 = 0):   #add the view function (index2)
+    return HttpResponse("value1 = "+str(val1))
+
+
+
+def viewbook(request, bookId):
+    # assume that we have the following books somewhere (e.g. database)
+    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    targetBook = None
+    if book1['id'] == bookId: targetBook = book1
+    if book2['id'] == bookId: targetBook = book2
+    context = {'book':targetBook} # book is the variable name accessible by the template
+    return render(request, 'bookmodule/show.html', context)
+
+
+def index(request):
+    return render(request, "bookmodule/index.html")
+ 
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+ 
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+ 
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+def links(request):
+    return render(request, 'bookmodule/links.html')
+
+def formatting(request):
+    return render(request, 'bookmodule/formatting.html')
+
+def listing(request):
+    return render(request, 'bookmodule/listing.html')
+
+def tables(request):
+    return render(request, 'bookmodule/tables.html')
+
+def search(request):
+    
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+        # now filter
+        books = __getBooksList()
+        newBooks = []
+        for item in books:
+            contained = False
+            if isTitle and string in item['title'].lower(): contained = True
+            if not contained and isAuthor and string in item['author'].lower():contained = True
+            
+            if contained: newBooks.append(item)
+        return render(request, 'bookmodule/bookList.html', {'books':newBooks})
+
+    return render(request, 'bookmodule/search.html')
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765,'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
+
+def simple_query(request):
+    mybooks = Book.objects.filter(title__icontains='and') # <- multiple objects
+    return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+
+
+def complex_query(request):
+    mybooks = books=Book.objects.filter(author__isnull = False).filter(title__icontains='and').filter(edition__gte = 2).exclude(price__lte = 100)[:10]
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')
+
+
+def task1(request):
+    books = Book.objects.filter(Q(price__lte = 80)) 
+    return render(request, 'bookmodule/bookList.html', {'books':books})
+
+
+def task2(request):
+    books = Book.objects.filter(Q(edition__gt = 3) & (Q(title__icontains = 'co') | Q(author__icontains = 'co')))
+    return render(request, 'bookmodule/bookList.html', {'books':books})
+
+
+def task3(request):
+    books = Book.objects.filter(~Q(edition__gt = 3) & (~Q(title__icontains = 'co') | ~Q(author__icontains = 'co')))
+    return render(request, 'bookmodule/bookList.html', {'books':books})
+
+
+def task4(request):
+    books = Book.objects.order_by('title')
+    return render(request, 'bookmodule/bookList.html', {'books':books})
+
+
+def task5(request):
+    query = Book.objects.aggregate(
+        count=Count('id'),
+        total_price=Sum('price'),
+        average_price=Avg('price'),
+        min_price=Min('price'),
+        max_price=Max('price')
+    )
+    return render(request, 'bookmodule/task5.html', {'query':query})
+
+
+
+def task7(request):
+    cities = Address.objects.annotate(student_count=Count('student'))
+    return render(request, 'bookmodule/task7.html', {'cities':cities})
